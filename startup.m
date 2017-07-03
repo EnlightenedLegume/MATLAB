ef0386e185ed59fe765dd1726ee51d5013a68c2e
Benjamin Huang <bdhuang@princeton.edu>
2017-07-03 15:46:36 -0400

    Updated startup file to work on multiple computers with environmental variables

--- startup.m
+++ startup.m
@@ -1,6 +1,11 @@
+
+% Add paths
+path2docs = getenv('DOCUMENTS');
+addpath(genpath(fullfile(path2docs,'MATLAB\Functions')));
+addpath(fullfile(path2docs,'MATLAB\Packages'));
+addpath(genpath(fullfile(path2docs,'MATLAB\Packages\matlab2tikz\src')));
+
+% Set plot defaults
 set(0,'DefaultLineLineWidth',1.5);
 set(0,'DefaultAxesFontSize',15);
-set(0,'DefaultTextInterpreter','LaTeX');
-addpath('C:\Users\bdhua\Documents\MATLAB\Functions')
-addpath('C:\Users\bdhua\Documents\MATLAB\Packages')
-addpath('C:\Users\bdhua\Documents\MATLAB\Packages\matlab2tikz\src');
+
