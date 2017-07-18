ce6a95e7664f9634f19c689c4c10270ef05d9b72
Benjamin Huang <bdhuang@princeton.edu>
2017-07-18 12:27:55 -0400

    Removed backslashes and used the 'fullfile' function

--- startup.m
+++ startup.m
@@ -1,9 +1,10 @@
 
 % Add paths
 path2docs = getenv('DOCUMENTS');
-addpath(genpath(fullfile(path2docs,'MATLAB\function')));
-addpath(fullfile(path2docs,'MATLAB\package'));
-addpath(genpath(fullfile(path2docs,'MATLAB\package\matlab2tikz\src')));
+addpath(genpath(fullfile(path2docs,'MATLAB','function')));
+addpath(fullfile(path2docs,'MATLAB','package'));
+addpath(genpath(fullfile(path2docs,'MATLAB','package', ...
+                         'matlab2tikz','src'))); 
 
 % Set plot defaults
 set(0,'DefaultLineLineWidth',1.5);
